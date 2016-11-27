76568ffc10c5a3140410b07d6a0884857b230fc4
tecokimura <tecokimura@gmail.com>
2016-11-27 14:08:42 +0900

    add phpunit test file

---
+++ tests/MemberTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kimura
+ * Date: 2016/11/27
+ * Time: 13:52
+ */
+class MemberTest extends PHPUnit_Framework_TestCase
+{
+    public function testAaa() {
+        $this->assertTrue(true);
+    }
+}
